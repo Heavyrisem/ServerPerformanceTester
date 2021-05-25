1d5672145868ee6851d40e7dba841f0fe99293a6
heavyrisem <pyo1748@gmail.com>
2021-05-25 21:55:05 +0900

    Test Code update

--- Looptester.ts
+++ Looptester.ts
@@ -1,29 +1,62 @@
-// const http = require('http');
-import http, {IncomingMessage, RequestOptions} from 'http';
+import net from 'net';
 
 let avgDelay = 0;
-// let errorCount = 0;
+let max = 0;
+let min = -1;
 let finishCount = 0;
-let Loop = 1000;
+let Connections = 100;
 
 const options = {
-	hostname: '52.231.155.110',
+	host: '52.231.183.177',
 	path: '/',
-	port: '3000',
-	method: "GET"
+	port: 80,
+	method: "GET",
+	Loop: 50
 };
 
-(() => {
-	console.log("Testing", options.hostname, options.port, options.path, options.method);
-	for (let i = 0; i <= Loop; i++) {
-		const Timer = Date.now();
-		function ResponseHandler(response: IncomingMessage) {
-			avgDelay += Date.now() - Timer;
-			finishCount++;
-			if (i == Loop) {
-				return console.log(avgDelay / Loop)
+function Requester(callback: Function): void {
+	const serverSocket = net.connect(options.port, options.host+((options.path == '/')? '' : options.path), () => {
+		let c = 0;
+		let Timer = Date.now();
+
+		serverSocket.on("error", (err) => {
+			console.log(err);
+		})
+
+		serverSocket.on("data", (b) => {
+			if (b.toString().split('\n')[0].indexOf('200') == -1) {
+				console.log("Error:", b.toString().split('\n'));
+				process.exit(1);
 			}
+			let time = Date.now() - Timer;
+			if (max < time) max = time;
+			if (min > time || min == -1) min = time;
+			avgDelay += time;
+			c++;
+			finishCount++;
+			if (c == options.Loop) {callback(); return serverSocket.end()};
+			SendReq();
+		});
+		
+		SendReq();
+
+		function SendReq() {
+			serverSocket.write(`${options.method} ${options.path} HTTP/1.1\r\n` +
+			'Accept: */*\r\n' + 
+			'Accept-Encoding: gzip, deflate\r\n' +
+			'Connection: keep-alive\r\n' +
+			`Host: ${options.host}\r\n` +
+			'User-Agent: TPS/1.0\r\n'+'\r\n');
 		}
-		http.request(options, ResponseHandler).end();
-	}
-})();
+	});
+};
+
+console.log("Creating Connection", options.host, options.port, options.path, options.method);
+console.log(Connections, "Connections,", options.Loop, "Requests");
+for (let i = 0; i < Connections; i++) {
+	Requester(() => {
+		if (finishCount == options.Loop*Connections) {
+			console.log("Avrage ResponseDelay:", parseFloat((avgDelay/finishCount).toFixed(2)), 'ms |', 'min:', min, 'ms |', 'max:', max, 'ms');
+		}
+	})
+}
--- tps.ts
+++ tps.ts
@@ -1,47 +1,54 @@
-import http, {IncomingMessage, RequestOptions} from 'http';
+import net from 'net';
 
 let avgDelay = 0;
-// let errorCount = 0;
 let Loop = 0;
-let MakeReq = true;
 let Timer = 0;
 
+
 const options = {
-	hostname: '52.231.155.110',
+	host: '52.231.183.177',
 	path: '/',
-	port: '3000',
+	port: 80,
 	method: "GET"
 };
 
 process.on('SIGINT', () => {
-    MakeReq = false;
     console.log("\nResult")
     CalculateTPS();
     process.exit();
 });
 
-function Req(options: RequestOptions): Promise<void> {
-    return new Promise(resolve => {
-        const Time = Date.now();
-        http.request(options, () => {
-            avgDelay += Date.now() - Time;
-            return resolve();
-        }).end();
-    })
-}
 
 function CalculateTPS() {
+    console.log(Loop)
     return console.log("TPS:", Math.round(Loop/((Date.now()-Timer)/1000)), '|', Math.round((Date.now() - Timer)/1000), 'Sec', '|', "Avarage ResponseTime:", parseFloat((avgDelay/Loop).toFixed(2)), "ms");
 }
 
 (async () => {
-	console.log("Testing", options.hostname, options.port, options.path, options.method);
-    Timer = Date.now();
+	console.log("Testing", options.host, options.port, options.path, options.method);
 
     setInterval(CalculateTPS, 1000);
-
-	while (MakeReq) {
-        await Req(options);
-        Loop++;
-	}
+    
+    const serverSocket = net.connect(options.port, options.host+((options.path == '/')? '' : options.path), () => {
+        serverSocket.on("data", (b) => {
+            console.log(b.toString());
+            if (b.toString().split('\n')[0].indexOf('200') == -1) {
+                console.log("Error:", b.toString().split('\n'));
+                process.exit(1);
+            }
+            avgDelay += Date.now() - Timer;
+            Loop++;
+            SendReq();
+        });
+        SendReq();
+        function SendReq() {
+            Timer = Date.now();
+            serverSocket.write(`${options.method} ${options.path} HTTP/1.1\r\n` +
+            'Accept: */*\r\n' + 
+            'Accept-Encoding: gzip, deflate\r\n' +
+            'Connection: keep-alive\r\n' +
+            `Host: ${options.host}\r\n` +
+            'User-Agent: TPS/1.0\r\n'+'\r\n');
+        }
+    })
 })();
